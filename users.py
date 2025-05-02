531b4e57da6640843974fc3b1ed812b774ba29cd
dnaveen88 <78192866+dnaveen88@users.noreply.github.com>
2025-05-02 16:28:11 +0530

    Update users.py
    qr code updated

--- users.py
+++ users.py
@@ -66,3 +66,94 @@ class UserManagement():
         new_key = secrets.token_hex(20)
         
         return new_key
+
+
+def remaining_wait_time(username, user_ip_address):
+    """
+    Check if the user account should be temporarily locked due to repeated failed login attempts.
+
+    Parameters:
+    - username (str): The username of the user for whom to check the wait time.
+
+    Returns:
+    - dict: A dictionary containing information about the wait time or a message to continue.
+    """
+
+    user = User.objects.get(username=username)
+    if user.consecutive_failed_login_cnt > 5:
+        domain = constants.USER_DOMAIN
+        action_type = constants.USER_LOCKED
+        system_user_obj = User.objects.get(username="system")
+        system_user_id = system_user_obj.id
+        create_log(domain, action_type, user.id, user.username, None, None, None, None,
+                None, None, system_user_id, user_ip_address)
+        whitelisting_ips = IPWhiteList.objects.filter(ipv4_addr=user_ip_address).exists()
+        if not whitelisting_ips:
+            failed_login_ip_obj, _ = FailedLoginIPList.objects.get_or_create(ipv4_addr=user_ip_address)
+            #incrementing consecutive login count for the IP address
+            failed_login_ip_obj.consecutive_failed_login_cnt += 1
+            failed_login_ip_obj.save()
+        if failed_login_ip_obj.consecutive_failed_login_cnt == constants.FAILED_IP_MAX_COUNT:
+            domain = constants.LOGIN_DOMAIN
+            action_type = constants.IPADDRESS_BLOCKED
+            create_log(domain, action_type, None, None, None, None, None, None,
+                        None, None, system_user_id, user_ip_address)
+        return {'errorMessage': 'Account is locked. Please contact the OSI Help Desk.'}, 423
+    
+    if user.consecutive_failed_login_cnt > 0:
+        elapsed_time = timezone.now() - user.last_login_failed_at
+        user_wait_time = {1: 1, 2: 2, 3: 4, 4: 8, 5: 20}
+        remaining_time = user_wait_time[user.consecutive_failed_login_cnt] * 15 - elapsed_time.seconds
+        remaining_minutes = seconds_to_minutes(remaining_time)
+        if remaining_time > 0:
+            return {'errorMessage': f'Login Failed. Please try after {remaining_minutes}',
+                    'remaining_seconds': remaining_time}, 429
+    return {'message': 'continue'}, 200
+def wait_time(username, user_ip_address):
+    """
+    Handles failed login attempts and calculates the wait time for subsequent attempts.
+
+    Args:
+        failed_login_response (dict): The response to be returned for a failed login attempt.
+        username (str): The username associated with the login attempt.
+        user_ip_address (str): The IP address of the user attempting to log in.
+
+    Returns:
+        tuple: A tuple containing a dictionary with either the error message or the wait time
+               and the corresponding HTTP status code.
+    """
+    # Add consecutive failed login count into FailedLoginIPList table.
+    system_user_obj = User.objects.get(username="system")
+    system_user_id = system_user_obj.id
+    whitelisting_ips = IPWhiteList.objects.filter(ipv4_addr=user_ip_address).exists()
+    if not whitelisting_ips:
+        failed_login_ip_obj, _ = FailedLoginIPList.objects.get_or_create(ipv4_addr=user_ip_address)
+        failed_login_ip_obj.consecutive_failed_login_cnt += 1
+        failed_login_ip_obj.save()
+    failed_login_ip_obj = FailedLoginIPList.objects.filter(ipv4_addr=user_ip_address).first()
+    if failed_login_ip_obj is not None and failed_login_ip_obj.consecutive_failed_login_cnt == constants.FAILED_IP_MAX_COUNT:
+        domain = constants.LOGIN_DOMAIN
+        action_type = constants.IPADDRESS_BLOCKED
+        create_log(domain, action_type, None, None, None, None, None, None,
+                    None, None, system_user_id, user_ip_address)
+    user= User.objects.get(username=username)
+    user.consecutive_failed_login_cnt = user.consecutive_failed_login_cnt+1
+    user.last_login_failed_at = timezone.now()
+    user.save()
+    user_wait_time = {1: 1, 2: 2, 3: 4, 4: 8, 5: 20}
+    if user.consecutive_failed_login_cnt > 5:
+        resp = {'errorMessage': \
+                'Account is locked. Please contact the OSI Help Desk.'}
+        domain = constants.USER_DOMAIN
+        action_type = constants.USER_LOCKED
+        create_log(domain, action_type, user.id, user.username, None, None, None, None, 
+                   None, None, system_user_id, user_ip_address)
+        status_code = 423
+    else:
+        remaining_seconds = 15 * user_wait_time[user.consecutive_failed_login_cnt]
+        remaining_minutes = seconds_to_minutes(remaining_seconds)
+        resp = {'errorMessage': \
+                f'Login Failed. Please try after {remaining_minutes}',
+                'remaining_seconds': remaining_seconds}
+        status_code = 429
+    return resp, status_code
